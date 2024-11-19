194b27bfe2feba3526539234fcae9035d63d754f
tknikhil <nikhiltk02@gmail.com>
2024-11-19 06:09:10 +0530

    completed JWT services

--- book-network/src/main/java/in/tkn/book_network/security/JwtService.java
+++ book-network/src/main/java/in/tkn/book_network/security/JwtService.java
@@ -1,4 +1,109 @@
 package in.tkn.book_network.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+@Value("${application.security.jwt.secret-key}")
+private String secretKey;
+
+    public String extractUserName(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims,T> claimsResolver) {
+        final Claims claims= extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+//        old approach
+//        return Jwts
+//                .parser()
+//                .setSigningKey(getSignInKey())
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody();
+
+//        new approach
+                return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    private  String generateToken(Map<String,Object> claims, UserDetails userDetails) {
+        return buildToken(claims,userDetails,jwtExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long jwtExpiration) {
+        var authorities= userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+//        old approach
+//        return Jwts.builder()
+//                .setClaims(extraClaims)
+//                .setSubject(userDetails.getUsername())
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis()+jwtExpiration))
+//                .claim("authorities",authorities)
+//                .signWith(getSignInKey())
+//                .compact();
+//        new approach
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+jwtExpiration))
+                .claim("authorities",authorities)
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()))&& !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token,Claims::getExpiration);
+    }
+
+
+    private SecretKey getSignInKey() {
+//        old approach
+//        byte [] keyBytes = Decoders.BASE64.decode(secretKey);
+//        return Keys.hmacShaKeyFor(keyBytes);
+//new approach
+        byte [] keyBytes = Base64.getDecoder().decode(secretKey.getBytes(StandardCharsets.UTF_8));
+        return new SecretKeySpec(keyBytes,"HmacSHA265");
+    }
 }
