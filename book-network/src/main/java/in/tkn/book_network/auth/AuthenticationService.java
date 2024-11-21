a53fe20b00b56f0e09fe5022f5a1e67767273b2a
tknikhil <nikhiltk02@gmail.com>
2024-11-21 07:46:46 +0530

    AuthenticationController

---
+++ book-network/src/main/java/in/tkn/book_network/auth/AuthenticationService.java
@@ -0,0 +1,9 @@
+package in.tkn.book_network.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void registerUser(RegistrationRequest requst) {
+    }
+}
---
+++ book-network/src/main/java/in/tkn/book_network/auth/RegistrationRequest.java
@@ -0,0 +1,31 @@
+package in.tkn.book_network.auth;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+    @NotEmpty(message="First Name is mandatory")
+    @NotBlank(message="First Name is mandatory")
+    private String firstName;
+    @NotEmpty(message="Last Name is mandatory")
+    @NotBlank(message="Last Name is mandatory")
+    private String lastName;
+    @Email(message="not a correct email")
+    @NotEmpty(message="Email Name is mandatory")
+    @NotBlank(message="Email Name is mandatory")
+    private String email;
+    @NotEmpty(message="Password Name is mandatory")
+    @NotBlank(message="Password Name is mandatory")
+    @Size(min=8,message = "Password should be minimum 8 char long")
+    private String password;
+}
